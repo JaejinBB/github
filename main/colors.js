43790226d1a4d215756e5efc5b5075ee1ee01085
JaejinBB <l_koon13@naver.com>
2021-04-07 08:20:53 +0900

    2104070820

--- main/colors.js
+++ main/colors.js
@@ -1,19 +1,22 @@
 var Links = {
   setColor:function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-      alist[i].style.color = color;
-      i = i + 1;
-    }
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+    $('a').css('color', color); // $('a')이 웹페이지의 모든 a태그를 jquery로 제어하겠다.
   }
 }
 var Body = {
   setColor:function (color){
-    document.querySelector('body').style.color = color;
+    //document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   },
   setBackGroundColor:function (color){
-    document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   }
 }
   function nightdayhandler(self){
@@ -23,7 +26,7 @@ var Body = {
       Body.setColor('white');
       self.value = 'day';
 
-      Links.setColor('powderblue');
+      Links.setColor('white');
     } else {
       Body.setBackGroundColor('white');
       Body.setColor('black');
